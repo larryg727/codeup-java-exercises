7814c917ec826896b9b2428ddf24c0fa9452f920
larryg727 <larrygonzales727@gmail.com>
2017-05-10 12:18:45 -0500

    -completed rain drop speak exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -43,26 +43,27 @@ public class ControlFlowExercises {
             }
         }
 
-        System.out.println("Enter the number you would like to go up to:");
-        int upTo = scan.nextInt();
-        scan.nextLine();   // take up extra enter from using nextInt
-        System.out.println("Would you like to continue?(Y/N)");
-        String cont = scan.nextLine();
-        if (cont.equalsIgnoreCase("y")){
-            System.out.println("OK, Here is your table then.");
-            System.out.println("");
-            System.out.println("");
-        System.out.println("number | squared | cubed");
-        System.out.println("------ | ------- | -----");
-        for(i=1; i <= upTo; i++){
-            int squared = i * i;
-            int cubed = i * i * i;
-            System.out.println("   " + i + "   |   " + squared + "     |   " + cubed + " ");
 
-        }}else{
-            System.out.println("Fine then.. Be that way ;)");
+        String cont = "Y";
+        while (cont.equalsIgnoreCase("y")) {
+            System.out.println("Enter the number you would like to go up to:");
+            int upTo = scan.nextInt();
+            scan.nextLine();   // take up extra enter from using nextInt
+
+            System.out.println("OK, Here is your table.");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("number | squared | cubed");
+            System.out.println("------ | ------- | -----");
+            for (i = 1; i <= upTo; i++) {
+                int squared = i * i;
+                int cubed = i * i * i;
+                System.out.println("   " + i + "   |   " + squared + "     |   " + cubed + " ");
+
+            }
+            System.out.println("Would you like to try another? Y/N");
+            cont = scan.nextLine();
         }
-
 
         String contGrade = "y";
         while (contGrade.equalsIgnoreCase("y")){
@@ -82,8 +83,40 @@ public class ControlFlowExercises {
             System.out.println("Would you like to check another grade? Y/N");
            contGrade = scan.next();
         }
-        System.out.println("Thank you, come again!");
 
+
+
+        String contRain = "Y";
+        while(contRain.equalsIgnoreCase("y")) {
+            System.out.println("Enter a number to convert it to raindrop:");
+            int rainInput = scan.nextInt();
+            String rainString = rainInput + "'s factors are ";
+
+            for (i = 1; i <= rainInput; i++) {
+                if (rainInput == i) {
+                    rainString += i + ".";
+                } else if (rainInput % i == 0) {
+                    rainString += i + " ,";
+                }
+            }
+            System.out.println(rainString);
+            String rainSpeak = " In raindrop-speak, this would be a ";
+            if (rainString.indexOf("3") != -1) {
+                rainSpeak += "Pling";
+            }
+            if (rainString.indexOf("5") != -1) {
+                rainSpeak += "Plang";
+            }
+            if (rainString.indexOf("7") != -1) {
+                rainSpeak += "Plong";
+            }
+            if (rainString.indexOf("3") != -1 || rainString.indexOf("5") != -1 || rainString.indexOf("7") != -1){
+                System.out.println(rainSpeak);
+            }
+            System.out.println("Would you like to try another number? Y/N");
+            contRain = scan.next();
+        }
+        System.out.println("Thank you, come again!");
 
     }
 }
